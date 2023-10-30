c4de239e3c0b270a7a4d2956612536a0b573cf7c
Som <aishwarysrivastava2006@gmail.com>
2023-10-31 01:09:30 +0530

    hello

--- components/Loading.js
+++ components/Loading.js
@@ -2,9 +2,9 @@ import React from "react";
 
 export default function Loading() {
   return (
-    <div className="flex flex-col fade cursor-none w-full min-h-screen bg justify-center items-center absolute z-50">
+    <div className="flex flex-col fade cursor-none w-full min-h-screen bg justify-center items-center fixed z-50">
       <div
-        className="flex z-50 flex-col h-[100px] cursor-none rounded-full circleoutline w-[100px]  absolute"
+        className="flex z-50 flex-col h-[100px] cursor-none rounded-full circleoutline w-[100px]  fixed"
       >
         <div className="circle-word text-white">I</div>
         <div className="circle-word text-white">I</div>
