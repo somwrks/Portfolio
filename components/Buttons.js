671f3e9e7ff480211a7004641dcb0b726a6f148f
Som S. <somwrks@gmail.com>
2024-05-27 15:17:35 +0530

    Workin

--- components/Buttons.js
+++ components/Buttons.js
@@ -7,19 +7,24 @@ export default function Buttons() {
       onClick={(e) => e.target.classList.contains("outer-box") && setShow("")}
       className="flex fade flex-col w-full min-h-screen fixed outer-box items-center backdrop-blur-lg z-50 "
     >
-      <div className="flex bg-gray-800 space-y-3 flex-col w-full md:w-1/2 md:mt-14  h-full p-4 items-start">
-        <div className="flex flex-row w-full  h-full bg-gray-600">
-          <button className="px-3  text-black text-xl  bg-gray-100">
+      <div className="flex fade bg-gray-800 space-y-3 flex-col w-full md:w-1/2 mt-14  min-h-[700px] md:min-h-[50vw]  items-start">
+        <div className="flex flex-row w-full justify-between h-full ">
+          <button onClick={() => setShow("resume")} className="w-full py-4 text-black text-xl  bg-gray-100">
             Resume
           </button>
-          <button className="px-3  text-white bg-black text-xl  ">
+          <button   onClick={() => setShow("freelance")} className="w-full py-4  text-white bg-black text-xl  ">
             Freelance
           </button>
         </div>
         {show == "resume" ? (
-          <div className="flex flex-col p-2 w-full"></div>
+          <div className="flex flex-col p-2 w-full h-full">
+            <h1 className="text-2xl text-white">Coming Soon...</h1>
+          </div>
         ) : (
-          <div className="flex flex-col p-2 w-full"></div>
+          <div className="flex flex-col p-2 w-full h-full">
+
+            <h1 className="text-2xl text-white">Coming Soon...</h1>
+          </div>
         )}
       </div>
     </div>
